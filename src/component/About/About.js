71851a24631305792e247161c4a80d48fdb089e2
Roldi christh MAMPASSI MOUKIETOU <christhmoukietou@gmail.com>
2023-11-20 10:25:52 +0100

    updates

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -1,35 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import logo from '../../logo.svg'
 import './abou.css'
+import { useInView } from 'react-intersection-observer';
 
 const About = () => {
-    const [isAbout, setIsAbout] = useState(false);
-    useEffect(()=>{
-        const handleScroll = ()=>{
-            setIsAbout(window.scrollY > window.innerHeight - 580)
-        }
+    
+    const { ref: title, inView: myElementVisible1} = useInView();
+    const { ref: content, inView: myElementVisible2} = useInView();
+    const { ref: btn, inView: myElementVisible} = useInView();
+   
 
-        //Appel initial pour définir l'état initial
-        handleScroll()
-        console.log(window.scrollY > 2*window.innerHeight);
-        //utilisation de setInterval pour vérifier périodiquement la position de défilement
-        const interval = setInterval(handleScroll, 100)
 
-        return ()=>{
-            //Nettoyage de l'interval lors du démontage du composant
-            clearTimeout(interval)
-        }
-    }, [])
     return (
-        <section className={isAbout ? 'about anime' : 'about'} id='apropos'>
-            <h2 className="heading"> A propos de <span>Moi</span></h2>
+        <section className={ 'about' } id='apropos'>
+            <h2 ref={title} className={myElementVisible1 ? "heading" : 'heading hide'}> A propos de <span>Moi</span></h2>
             <div className='about-img'>
                 <img src={logo} alt="" />
                 <span className="circle-spin"></span>
             </div>
             <div className="about-content">
                 <h3>Développeurr Web & Mobile !</h3>
-                <p>
+                <p ref={content} class={myElementVisible2 ? 'anime' : 'hide'}>
                     Actuellement en 2e année, je suis un étudiant passionné par le développement web 
                     et mobile. j'ai choisi ce domaine en raison de mon intérêt pour les technologies 
                     de l'information et la manière dont elles peuvent être utilisées pour résoudre 
@@ -40,8 +31,8 @@ const About = () => {
                     des solutions innovantes. En tant qu'étudiant en développement web et mobile, 
                     je suis bien équipé pour réussir dans un monde numérique en constante évolution.
                 </p>
-                <div className='btn-box box_center'>
-                    <button className="btn btn1">Télécharger CV</button>
+                <div ref={btn} className='btn-box box_center'>
+                    <button className={myElementVisible ?  'btn btn1 anime' :"btn btn1"}>Télécharger CV</button>
                 </div>
             </div>
         </section>
