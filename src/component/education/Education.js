ba73a229f84b24ca6eaa472172283258740589de
Roldi christh MAMPASSI MOUKIETOU <christhmoukietou@gmail.com>
2023-11-21 15:01:32 +0100

    updates

--- src/component/education/Education.js
+++ src/component/education/Education.js
@@ -1,18 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React  from 'react';
 import {AiFillCalendar} from 'react-icons/ai'
 import './education.css'
 import { useInView } from 'react-intersection-observer';
 
 const Education = () => {
     const { ref: title, inView: isEducation} = useInView();
+    const { ref: education, inView: isEducate} = useInView();
+    const { ref: educ_box1, inView: isEducate_box1} = useInView();
+    const {  ref: educ_box2, inView: isEducate_box2} = useInView();
+    const { ref: experience, inView: isExpert } = useInView();
     return (
-        <section ref={title} className={isEducation ? 'education anime' : 'education'} id='education'>
-            <h2>Mon <span>Parcours</span></h2>
+        <section className={'education'} id='education'>
+            <h2 ref={title} className={isEducation && 'title_show'}>Mon <span>Parcours</span></h2>
             <div className='education-row'>
                 <div className='education-column'>
-                    <h3 className="title">éducation</h3>
+                    <h3 ref={education} className={isEducate ? "title left" : 'hide'}>éducation</h3>
                     <div className="education-box">
-                        <div className="education-content">
+                        <div ref={educ_box1} className={isEducate_box1 ? "education-content animate box1" : 'hide'}>
                             <div className="content">
                                 <div className="year"><AiFillCalendar/> 2020 - 2021</div>
                                 <h3>Licence Mathématiques - Université Marien Ngouabi</h3>
@@ -24,7 +28,7 @@ const Education = () => {
                                 </p>
                             </div>
                         </div>
-                        <div className="education-content">
+                        <div ref={educ_box2} className={isEducate_box2 ? "education-content animate box2" : 'hide'}>
                             <div className="content">
                                 <div className="year"><AiFillCalendar/> 2021 - Auj</div>
                                 <h3>Bachelor Developpement web - Ecole Multimédia de paris</h3>
@@ -39,7 +43,7 @@ const Education = () => {
                     </div>
                 </div>
                 <div className='education-column'>
-                    <h3 className="title right">expérience</h3>
+                    <h3 ref={experience} className={isExpert ? "title right" : 'hide'}>expérience</h3>
                     <div className="education-box">
                         <div className="education-content">
                             <div className="content">
